14f929c348f149079db22b372610a44dc16fe81e
vimlinuz <vimlinuz@gmail.com>
2026-07-11 01:46:44 +0545

    types: added more custom error types to handle the errors in efficient way

--- programs/solpoll/src/error.rs
+++ programs/solpoll/src/error.rs
@@ -1,7 +1,19 @@
 use anchor_lang::prelude::*;
 
 #[error_code]
-pub enum ErrorCode {
+pub enum PollError {
+    #[msg("Invalid poll duration")]
+    InvalidPollDuration,
+
+    #[msg("Invalid result visible duration")]
+    InvalidResultVisibleDuration,
+
+    #[msg("Invalid title")]
+    InvalidTitle,
+
+    #[msg("Invalid description")]
+    InvalidDescription,
+
     #[msg("Custom error message")]
     CustomError,
 }
